2c38853bf76e5eca86dd9821075efc08e5d61711
JhowQT <jhon.q.torrez@gmail.com>
2025-11-14 21:21:02 -0300

    criação das classes controller e configuração da properties para fazer a conexão com o banco de dados

--- src/main/java/fiap/com/br/start_trek/controller/TipoUsuarioController.java
+++ src/main/java/fiap/com/br/start_trek/controller/TipoUsuarioController.java
@@ -1,5 +1,40 @@
 package fiap.com.br.start_trek.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import fiap.com.br.start_trek.entity.*;
+import fiap.com.br.start_trek.service.*;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/tipo-usuario")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class TipoUsuarioController {
     
+    private final TipoUsuarioService tipoUsuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<TipoUsuario>> listarTodos(){
+        List<TipoUsuario> tipos = tipoUsuarioService.listarTodos();
+        if(tipos.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(tipos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Long id){
+        try {
+            TipoUsuario tipoUsuario = tipoUsuarioService.buscarPorID(id);
+            return ResponseEntity.ok(tipoUsuario); // Aqui vem o 200
+        } catch (Exception e) {
+            return ResponseEntity
+                .status(404)
+                .body("Tipo de usuario não foi encontrado com este ID: " + id);
+        }
+    }
 }
